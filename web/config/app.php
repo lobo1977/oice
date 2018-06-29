333889ea1079de5c41101c04a5d22634a3d0dc83
LOBO-GIT\lobo <buchunguang@git.com.cn>
2018-06-29 10:05:51 +0800

    网站标题修改为“商办云”

--- web/config/app.php
+++ web/config/app.php
@@ -15,7 +15,7 @@
 
 return [
     // 应用名称
-    'app_name'               => 'Oice 商办云',
+    'app_name'               => '商办云',
     // 应用地址
     'app_host'               => 'm.o-ice.com',
     // 应用调试模式
