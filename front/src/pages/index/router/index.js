c5cb629eb6853e4ef54030d0409b0ddd6ddc9af7
lobo1977 <lobo1977@qq.com>
2018-07-28 18:26:19 +0800

    修复路由缺陷

--- front/src/pages/index/router/index.js
+++ front/src/pages/index/router/index.js
@@ -16,6 +16,10 @@ export default new Router({
       path: '/',
       name: 'Home',
       redirect: '/building'
+    },
+    {
+      path: '/index.html',
+      redirect: '/building'
     }, {
       path: '/building',
       name: 'Building',
