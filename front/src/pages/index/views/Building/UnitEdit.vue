03c87e5ce30e2f3fb19bda6fdf432cb61a226d5f
LOBO-GIT\lobo <buchunguang@git.com.cn>
2019-05-13 17:42:27 +0800

    代码优化

--- front/src/pages/index/views/Building/UnitEdit.vue
+++ front/src/pages/index/views/Building/UnitEdit.vue
@@ -60,7 +60,7 @@
           @on-change="validateForm" @on-click-error-icon="mobileError" :should-toast-error="false"></x-input>
       </group>
 
-      <group v-if="info.user_id == user.id" gutter="10px" label-width="4em" label-margin-right="1em" label-align="right">
+      <group v-if="info.user_id == user.id || info.user_id === 0" gutter="10px" label-width="4em" label-margin-right="1em" label-align="right">
         <cell title="所属企业" @click.native="selectCompany" :is-link="companyPickerData.length != 1" 
             :value="companyText" value-align="left"></cell>
         <x-switch title="是否公开" inline-desc="是否全网可见" v-model="info.share"></x-switch>
