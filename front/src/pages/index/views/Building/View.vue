b5e68366251bc697a17f387d03a107d0be2a9427
lobo <lobo1977@qq.com>
2019-06-17 21:53:54 +0800

    增加项目地址栏定位图标

--- front/src/pages/index/views/Building/View.vue
+++ front/src/pages/index/views/Building/View.vue
@@ -26,8 +26,10 @@
     <div v-show="tab === 0 && !showPush">
       <group gutter="0" label-width="4em" label-margin-right="1em" label-align="right">
         <cell title="项目类型" value-align="left" :value="info.building_type" v-show="info.building_type"></cell>
-        <cell title="地址" value-align="left" :value="info.area + info.address" v-show="info.area || info.address" 
-          :is-link="true" @click.native="showMap = true"></cell>
+        <cell value-align="left" :value="info.area + info.address" v-show="info.area || info.address" 
+          :is-link="true" @click.native="showMap = true">
+          <span slot="title"><x-icon type="android-pin" size="22" style="position:relative;top:4px;"></x-icon> <span>地址</span></span>
+        </cell>
         <cell title="竣工日期" value-align="left" :value="info.completion_date|formatDate" v-show="info.completion_date"></cell>
         <cell title="租售" value-align="left" :value="info.rent_sell" v-show="info.rent_sell"></cell>
         <cell title="价格" value-align="left" :value="info.price" v-show="info.price"></cell>
