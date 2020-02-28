589de49abc3efbce2b50c10caf24f92328b1d1cb
卜春光 <buchunggung@git.com.cn>
2020-02-28 21:55:55 +0800

    关闭夜间机器人签到回复

--- web/application/common/Wechat.php
+++ web/application/common/Wechat.php
@@ -446,9 +446,14 @@ class Wechat {
 				Robot::signIn($robot_id, 
 					$this->user['openid'], 
 					$this->user['headimgurl']);
-				$content = '签到成功。机器人' . $this->user['nickname'] . 
-					'已经在线，如需群发推广，转到您要推广的项目页，点击“群发推广”。<a href="https://' . 
-					config('app_host') . '/app/my/building">转到我的项目页</a>。';
+				$hour = intval(date('H', time()));
+				if ($hour > 7 && $hour < 22) {
+					$content = '签到成功。机器人' . $this->user['nickname'] . 
+						'已经在线，如需群发推广，转到您要推广的项目页，点击“群发推广”。<a href="https://' . 
+						config('app_host') . '/app/my/building">转到我的项目页</a>。';
+				} else {
+					$content = "";
+				}
 			}
 		}
 		
